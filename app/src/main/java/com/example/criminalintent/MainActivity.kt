0942f1a91f4a13766d3784144734eb5b144c1cc9
Melissa Heredia <herediamelissa19@gmail.com>
2020-04-11 21:03:22 -0400

    Add recycler view

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val currentFragment= supportFragmentManager.findFragmentById(R.id.fragment_container)
         if(currentFragment==null){
-            val crimeFragment= CrimeFragment()
+            val crimeFragment= CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, crimeFragment)
